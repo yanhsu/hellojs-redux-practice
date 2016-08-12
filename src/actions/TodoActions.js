aefbd65add8234d6d95b272930e44d1e5dd3ce29
yanhsu <e31217@gmail.com>
2016-08-12 15:33:23 +0800

    update practice

--- src/actions/TodoActions.js
+++ src/actions/TodoActions.js
@@ -1,4 +1,17 @@
 // action type
-
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
 
 // action creator
+export  function addTodo(text){
+  return{
+    type: ADD_TODO,
+    text:text
+  }
+}
+export  function toggleTodo(index){
+  return{
+    type: TOGGLE_TODO,
+    index
+  }
+}
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { addTodo, toogleTodo } from 'actions/TodoActions'
+import { addTodo, toggleTodo } from 'actions/TodoActions'
 import AddTodo from 'components/Todo/AddTodo'
 import TodoList from 'components/Todo/TodoList'
 
@@ -9,8 +9,10 @@ class App extends Component {
     const { dispatch, todos } = this.props
     return (
       <div>
-        
+        <AddTodo onAddClick={text=>dispatch(addTodo(text))}/>
+        <TodoList todos={todos} onTodoClick={index=>dispatch(toggleTodo(index))}/>
       </div>
+
     )
   }
 }
